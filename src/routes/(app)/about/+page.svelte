cfd8c691ec1f1dff18ecb20877bbe182ec9eb50c
PuurfectDev-MD <new.manishd@gmail.com>
2026-03-23 22:15:44 +0545

    Added tiptap editor and made changes to user page

--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -9,11 +9,19 @@
 	const chartData = $derived(data.postsPerMonth);
 </script>
 
-<h1>This is the user info page</h1>
-<div class="flex flex-col gap-y-6 bg-red-400 p-4">
-	<h1>Username: {data.user.user_metadata.username}</h1>
-	<h1>Email: {data.user.email}</h1>
+<div class="grid max-h-[80%] grid-cols-2">
+	<div>
+		<img src="/images/image.png" class="w-fit p-3" />
+	</div>
+
+	<div class="flex flex-col justify-start gap-y-6 bg-red-400 p-4 pt-10">
+		<h1>Username: {data.user.user_metadata.username}</h1>
+		<h1>Email: {data.user.email}</h1>
+		<h1>Email: {data.user.email}</h1>
+		<h1>Email: {data.user.email}</h1>
+	</div>
 </div>
+
 {#if mounted}
 	<LineChart
 		class="mt-5 bg-white p-4 text-black"
--- src/routes/(app)/create/+page.svelte
+++ src/routes/(app)/create/+page.svelte
@@ -1,12 +1,58 @@
+<script lang="ts">
+	import { Editor } from '@tiptap/core';
+	import Image from '@tiptap/extension-image';
+	import StarterKit from '@tiptap/starter-kit';
+	import { onMount } from 'svelte';
+	import './style.css';
+	let element: HTMLDivElement;
+	let editor;
+
+	onMount(() => {
+		editor = new Editor({
+			element: element,
+			extensions: [
+				StarterKit.configure({
+					heading: {
+						levels: [1, 2, 3],
+						HTMLAttributes: {
+							class: 'editor_heading'
+						}
+					},
+					undoRedo: false,
+					paragraph: {
+						HTMLAttributes: {
+							class: 'editor_paragraph'
+						}
+					}
+				}),
+				Image
+			],
+			content: `
+		<h2>
+              Hi there,
+            </h2>
+            <p>
+              this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+            </p>
+            <ul>
+              <li>
+                That’s a bullet list with one …
+              </li>
+              <li>
+                … or two list items.
+              </li>
+            </ul>
+            <p>
+              Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+            </p>
+		`,
+			autofocus: true, // make the text editable
+			editable: true, // make the text editable
+			injectCSS: false
+		});
+	});
+</script>
+
 <h1>Create your next big thing</h1>
 
-<div class="flex justify-center gap-x-5 p-8">
-	<div class="flex flex-col gap-y-8">
-		<a href="/posts/new"><h1>+</h1></a>
-		<h1>New note</h1>
-	</div>
-	<div>
-		<a href="/"><h1>+</h1></a>
-		<h1>Coming soon</h1>
-	</div>
-</div>
+<div bind:this={element} class="w-screen"></div>
