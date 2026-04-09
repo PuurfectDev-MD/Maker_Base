f12aa042abf608ab4ca1ccbd863463d3ad972422
PuurfectDev-MD <new.manishd@gmail.com>
2026-04-09 22:31:30 +0545

    MAde changes to the front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="main mt-20 flex flex-col items-center">
+	<h1>Your Maker Stuff,</h1>
+	<h1>Documented for forever</h1>
 
-<h1 class="text-6xl">THIS IS THE MAIN PAGE</h1>
+	<p class="mt-8 max-w-md text-center">
+		Write, explore detailed notes and revist components, concepts, how-to-do-expanations all at one
+		place :)
+	</p>
+</div>
+
+<div class="mt-8 flex justify-center">
+	<div class="group flex flex-col">
+		<a href="/explore" class="mx-4 border-2 p-3 text-center no-underline!">Explore</a>
+		<p class="mt-2 hidden text-[--text-muted] transition-opacity group-hover:block">
+			Promise they good :)
+		</p>
+	</div>
+	<div class="group flex flex-col">
+		<a href="/auth/signup" class="mx-4 border-2 p-3 text-center no-underline!">Signup</a>
+		<p class="mt-2 hidden transition-opacity group-hover:block">Join in the fun!</p>
+	</div>
+</div>
+
+<div class="mt-16 flex items-center gap-3 pt-4">
+	<div class="h-px flex-grow bg-[var(--accent)]"></div>
+	<p class="shrink-0 text-sm text-[var(--text-muted)]">A taste of what's inside</p>
+	<div class="h-px flex-grow bg-[var(--accent)]"></div>
+</div>
+
+<div class="-z-index-5 mt-8 flex grow flex-row gap-x-4 p-4 opacity-50">
+	<div class="card">
+		<h1>TFT Screens - OLED vs TFT</h1>
+		<p>........................</p>
+		<p>........................</p>
+		<p>........................</p>
+	</div>
+
+	<div class="card">
+		<h1>How to boost converter</h1>
+		<p>........................</p>
+		<p>........................</p>
+
+		<p>........................</p>
+	</div>
+
+	<div class="card">
+		<h1>Supabase + Svelte Kit</h1>
+		<p>........................</p>
+		<p>........................</p>
+		<p>........................</p>
+	</div>
+</div>
+
+<div class="flex items-center justify-center"></div>
