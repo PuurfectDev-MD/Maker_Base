2d93daeabb6372652fd8476f961b3ff4ae61882e
PuurfectDev-MD <new.manishd@gmail.com>
2026-05-08 20:54:06 +0545

    Vercel analytics integration

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,7 @@
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+import injectAnalytics from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import { dev } from '$app/environment';
 injectSpeedInsights();
+injectAnalytics.inject({ mode: dev ? 'development' : 'production' });
+
